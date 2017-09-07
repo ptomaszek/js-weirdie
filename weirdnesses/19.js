091dbe809d6b8cea1445fd99a3b8986f9738ffc3
Pawel Tomaszek <p.tomaszek.cs@gmail.com>
2017-09-07 15:46:44 +0200

    another weirdness

---
+++ weirdnesses/19.js
@@ -0,0 +1,8 @@
+function isTrue(value) { return !!value; }
+
+printThis(isTrue("-1"));
+printThis(isTrue(-1));
+printThis(isTrue("1"));
+printThis(isTrue(1));
+printThis(isTrue("0"));
+printThis(isTrue(0));
